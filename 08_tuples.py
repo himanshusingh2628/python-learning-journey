70cd38cb128bd529501aba77e891825835ea1617
Himanshu Singh <154902783+himanshusingh2628@users.noreply.github.com>
2026-08-17 01:06:58 +0530

    Add example of Python tuples usage

---
+++ 08_tuples.py
@@ -0,0 +1,10 @@
+# Python Tuples
+
+languages = ("Python", "C", "Java", "SQL")
+
+print("Languages:")
+for language in languages:
+    print(language)
+
+print("\nFirst Language:", languages[0])
+print("Total Languages:", len(languages))
