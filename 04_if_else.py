bdfbce0f4324b1f1e96d9395e4957c6b9e3a05ee
Himanshu Singh <154902783+himanshusingh2628@users.noreply.github.com>
2026-08-17 01:04:28 +0530

    Add age and number evaluation with if-else statements

---
+++ 04_if_else.py
@@ -0,0 +1,17 @@
+# Python If-Else
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote yet.")
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("Positive number")
+elif number < 0:
+    print("Negative number")
+else:
+    print("Zero")
